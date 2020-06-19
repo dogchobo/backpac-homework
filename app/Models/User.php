8983395748718b5c7ecd921de80052f2ec4ef1a1
RYAN <jhchoi@seoulpass.kr>
2020-06-19 23:56:36 +0900

    1. 엘로퀀트 모델 저장용 디렉토리 생성 2. 기본 User 모델 신규 경로로 이동 및 Auth 설정 기본 User 모델 경로 변경 3. Order 모델 추가

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,23 +1,17 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
-
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
+    protected $fillable = [];
 
     /**
      * The attributes that should be hidden for arrays.
@@ -25,7 +19,8 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password',
+        'api_token',
     ];
 
     /**
@@ -33,7 +28,5 @@ class User extends Authenticatable
      *
      * @var array
      */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    protected $casts = [];
 }
